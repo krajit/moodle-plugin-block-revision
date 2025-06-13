fc0ee1bad0b99d53639364d4931e4f8ffa8c46a4
Ajit Kumar <ajitkumar22@gmail.com>
2025-06-13 10:36:18 +0000

    form saving not running

--- ajax.php
+++ ajax.php
@@ -4,27 +4,45 @@ require_login();
 
 $action = required_param('action', PARAM_ALPHA);
 $userid = required_param('userid', PARAM_INT);
+$pageurl = required_param('pageurl', PARAM_TEXT);
 require_sesskey();
 
-$record = $DB->get_record('your_table', ['userid' => $userid], '*', MUST_EXIST);
+$record = $DB->get_record('block_plugin_tracker', [
+    'userid' => $userid,
+    'pageurl' => $pageurl
+]);
+
+if (!$record) {
+    $record = new stdClass();
+    $record->userid = $userid;
+    $record->pageurl = $pageurl;
+    $record->learninglevel = null;
+    $record->revisioncount = 0;
+    $record->nextreview = null;
+    $record->timemodified = time();
+    $record->id = $DB->insert_record('block_plugin_tracker', $record);
+}
 
 switch ($action) {
     case 'update_learning_level':
         $level = required_param('level', PARAM_ALPHA);
         $record->learninglevel = $level;
-        $DB->update_record('your_table', $record);
+        $record->timemodified = time();
+        $DB->update_record('block_plugin_tracker', $record);
         break;
 
     case 'update_revision':
         $revision = required_param('revision', PARAM_INT);
         $record->revisioncount = $revision;
-        $DB->update_record('your_table', $record);
+        $record->timemodified = time();
+        $DB->update_record('block_plugin_tracker', $record);
         break;
 
     case 'update_review_date':
-        $nextreview = required_param('nextreview', PARAM_TEXT); // format: YYYY-MM-DD
+        $nextreview = required_param('nextreview', PARAM_TEXT);
         $record->nextreview = $nextreview;
-        $DB->update_record('your_table', $record);
+        $record->timemodified = time();
+        $DB->update_record('block_plugin_tracker', $record);
         break;
 }
 
