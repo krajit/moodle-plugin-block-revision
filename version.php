c556f4d196c870cffd2f5be4ef9f9d1e7b9f9394
Ajit Kumar <ajitkumar22@gmail.com>
2025-06-15 11:35:40 +0000

    version change

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_revision';
 $plugin->release = '0.1.0';
-$plugin->version = 2025061205;
+$plugin->version = 2025061209;
 $plugin->requires = 2025040800;
 $plugin->maturity = MATURITY_ALPHA;
